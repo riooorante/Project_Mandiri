8b41bc377f9be8242d8e8fc8fe7bfe4440027be7
riooorante <rantetadungrio@gmail.com>
2022-11-21 23:29:52 +0800

    Manajemen-2

--- Manjemen_Keuangan_Sederhana/Main.py
+++ Manjemen_Keuangan_Sederhana/Main.py
@@ -1,10 +1,19 @@
 import datetime
-from prettytable import prettytable
+from tabulate import tabulate
 while True:
-    x = datetime.datetime.now()
-    _Keterangan = input("Keterangan : ")
-    _pengeluaran = int(input("Pengeluaran : "))
-    _kas = int(input("Kas : "))
+    x = datetime.datetime.now() 
+    _fase = "Start"
+    while _fase =="Start":
+        _Keterangan = input("Keterangan : ")
+        _pengeluaran = input("Pengeluaran : ")
+        with open('data.txt', "a") as catat:
+            _data = [[x.strftime("%x").ljust(22), _Keterangan.ljust(47), _pengeluaran.ljust(16)]] 
+            catat.write(tabulate(_data,tablefmt="grid"))
+        _fase = "Stop"
+    print("Terima kasih...")
+    break
+
+        
 
     
 
