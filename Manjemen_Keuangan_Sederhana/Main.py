51e3123ffbf6ea71705e18b393526994d7c911f0
riooorante <rantetadungrio@gmail.com>
2022-11-21 22:38:44 +0800

    Manajemen1

--- Manjemen_Keuangan_Sederhana/Main.py
+++ Manjemen_Keuangan_Sederhana/Main.py
@@ -1,1 +1,11 @@
-g = input("Bulan :")
+import datetime
+from prettytable import prettytable
+while True:
+    x = datetime.datetime.now()
+    _Keterangan = input("Keterangan : ")
+    _pengeluaran = int(input("Pengeluaran : "))
+    _kas = int(input("Kas : "))
+
+    
+
+    
